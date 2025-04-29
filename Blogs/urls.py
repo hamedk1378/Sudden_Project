94e645427471d7750ef065f0cf22719520d8f842
hamed.k1378 <hamed.k1378@gmail.com>
2025-04-29 07:21:17 -0400

    Added views, templates and url routes for Idea index and detail pages in Blogs app

--- Blogs/urls.py
+++ Blogs/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import BlogsHomeView
+from . import views
 
-
+app_name = "blogs"
 urlpatterns= [
-        path("", BlogsHomeView, name="blogs_home"),
-
+        path("", views.blogs_home_view, name="blogs_home"),
+        path("index/", views.ideas_index_view, name="idea_index"),
+        path("<int:idea_id>/", views.idea_detail_view, name="idea_detail"),
         ]
 
 
--- Blogs/views.py
+++ Blogs/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from . import models as b_models
 
-def BlogsHomeView(request):
+def blogs_home_view(request):
     return HttpResponse("Sudden Project`s Blogs Home")
+
+
+
+def ideas_index_view(request):
+    idea_q = b_models.Idea.objects.order_by("-pub_date")[:3]
+    context = {"idea_list" : idea_q}
+    return render(request, "Blogs/idea_index.html", context)
+
+
+def idea_detail_view(request, idea_id):
+    idea_ob = get_object_or_404(b_models.Idea, pk = idea_id)
+    return render(request, "Blogs/idea_detail.html", {"idea_ob" : idea_ob})
