2e3fa203a64d987bd43a2fd768a499dbf9914572
hamed.k1378 <hamed.k1378@gmail.com>
2025-04-29 07:06:33 -0400

    Created an abstract BaseContent model in the Blogs app; Comment (new) and Idea models now inherit from it.

--- Blogs/models.py
+++ Blogs/models.py
@@ -5,18 +5,35 @@ from datetime import timedelta
 
 
 
-
-class Idea(models.Model):
-    #any Idea that user wish to share with others
-    topic = models.CharField(max_length=256)
+class BaseContent(models.Model):
+    #
     body = models.TextField()
+    likes = models.IntegerField(default=0)
     user = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return str(self.user.username + f" 's Idea: {self.topic}")
+    class Meta:
+        abstract = True
 
     def is_recently_published(self):
         return self.pub_date >= timezone.now() - timedelta(days = 3)
+
+
+class Idea(BaseContent):
+    #any Idea that user wish to share with others
+    topic = models.CharField(max_length=256)
+
+    def __str__(self):
+        return str(self.user.username + f" 's Idea: {self.topic}")
+
+
+class Comment(BaseContent):
+    #People's comment on a Idea
+    idea = models.ForeignKey(Idea, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return str(self.user.username + f" 's comment on: {self.idea.topic}")
+
+
 
