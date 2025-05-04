c8ceed3a4791f6407a0b5bdbcca2449514dd79e9
hamed.k1378 <hamed.k1378@gmail.com>
2025-05-04 13:30:26 -0400

    Created a BaseContentAdmin class

--- Blogs/admin.py
+++ Blogs/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Idea
 
-admin.site.register(Idea)
+
+class BaseContentAdmin(admin.ModelAdmin):
+    fieldsets = [
+            [None, {"fields": ["body", "likes"]}],
+            ["Date Information", {"fields": ["pub_date", "last_update"]}],
+            ["Writer", {"fields": ["user"]}],
+    ]
+    list_filter = ["pub_date", "user__username"]
+    readonly_fields = ["pub_date", "last_update"]
+
+
+class IdeaAdmin(BaseContentAdmin):
+    BaseContentAdmin.fieldsets[0][1]["fields"].insert(0, "topic")
+    list_display = ["user__username", "topic", "pub_date", "is_recently_published"]
+
+
+admin.site.register(Idea, IdeaAdmin)
 
